93df1cfb1aa3c0f554fd0ba25c847ee0a72b0a5a
Jaganprafful <67191031+Jaganprafful@users.noreply.github.com>
2021-09-12 16:30:36 -0500

    Update settings.py

--- clientms/settings.py
+++ clientms/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a2+!g+lzqjg_^#-db(w)d7y+b_gzwf0($5l1=)!7ud_73ki2cd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 AUTH_USER_MODEL = 'users.CustomUser'
 # Application definition
@@ -119,8 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'prafful104@gmail.com'
