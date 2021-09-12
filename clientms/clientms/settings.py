522ce37565a48ded260a163375349e43ad8dd266
Jaganprafful <67191031+Jaganprafful@users.noreply.github.com>
2021-09-12 16:12:10 -0500

    Update settings.py

--- clientms/clientms/settings.py
+++ clientms/clientms/settings.py
@@ -122,9 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'statics')
-]
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'prafful104@gmail.com'
